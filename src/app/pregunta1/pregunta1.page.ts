00bd338c13a8a117c4032c2e52347cedf7d1e96f
darkalexpp <darkalexpp@gmail.com>
2021-07-29 11:27:17 -0500

    Optimizar pregunta1

--- src/app/pregunta1/pregunta1.page.ts
+++ src/app/pregunta1/pregunta1.page.ts
@@ -8,24 +8,10 @@ import {recurso} from 'src/app/clases/recurso';
 })
 export class Pregunta1Page implements OnInit {
 
-
   arreglo_recursos : recurso [];
+  nimg=12;
 
-  arreglo_informacion =
-  [
-    ['','https://image.freepik.com/vector-gratis/casa-dos-pisos_1308-16176.jpg','diraud1','diraud2','diraud3'],
-    ['','https://www.freude-kinder.com/wp-content/uploads/2020/06/7-1.jpg','diraud1','diraud2','diraud3'],
-    ['','https://hispanicla.com/wp-content/uploads/2010/09/Pinochet.jpg','diraud1','diraud2','diraud3'],
-    ['','https://static01.nyt.com/newsgraphics/2019/08/01/candidate-pages/7d63f01f112e79da7ac60c0448a4047a155ff410/trump.jpg','diraud1','diraud2','diraud3'],
-    ['','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaYDXhC8h4rm4DuCZ8K1CjAluWxqJwMZgRZQ&usqp=CAU','diraud1','diraud2','diraud3'],
-    ['','https://i.ytimg.com/vi/rXQzsQj9noQ/maxresdefault.jpg','diraud1','diraud2','diraud3'],
-    ['','https://estaticos.serpadres.es/media/cache/1140x_thumb/uploads/images/article/5fae74835bafe827219e8ace/series_0.jpg','diraud1','diraud2','diraud3'],
-    ['','https://www.eltiempo.com/files/article_multimedia/uploads/2018/02/16/5a87aa2aab054.jpeg','diraud1','diraud2','diraud3'],
-    ['','https://decine21.com/media/com_decine21/listas/100074/54d51d075bb59603857103d1c1dc1978-780.jpg','diraud1','diraud2','diraud3'],
-    ['','https://www.pocoyo.com/img/Categorias/Adivinanzas/2018/01/thumb-adivinanzas-dificiles.jpg','diraud1','diraud2','diraud3'],
-    ['','https://files.adventistas.org/noticias/es/2019/07/26110751/livros-de-ellen-white-inspiram-serie-animada-para-criancas.jpg','diraud1','diraud2','diraud3'],
-    ['','https://s1.eestatic.com/2021/05/06/imprescindibles/579204207_184285862_1024x576.jpg','diraud1','diraud2','diraud3']
-  ];
+  ra= ['barco','cangrejo','caramelo','conejo','escalera','escoba','lokidrilo','murcielago','oso','pera','sandalia','telefono'];
 
   arreglo_preguntas = [];
 
@@ -154,6 +140,9 @@ export class Pregunta1Page implements OnInit {
       }
       else{
         alert("vales vrg guambra")
+        let audio = new Audio('assets/audio/short-circuit.mp3');
+        audio.load();
+        audio.play();
       }
     }
     //-------------------------------------------------------------------------------------
@@ -161,14 +150,14 @@ export class Pregunta1Page implements OnInit {
     crearRecursos() //convierte el arreglo de ifnormacion en un arreglo de recursos
     {
       var arr = [];
-      for(let i =0;i< this.arreglo_informacion.length;i++)
+      for(let i =0;i< this.ra.length;i++)
       {
         var r = new recurso;
-        r.nombre = this.arreglo_informacion[i][0];
-        r.dirImagen = this.arreglo_informacion[i][1];
-        r.audioPalabra = this.arreglo_informacion[i][2];
-        r.audioSilabas = this.arreglo_informacion[i][3];
-        r.audioLetras = this.arreglo_informacion[i][4];
+        r.nombre = this.ra[i];
+        r.dirImagen = '/assets/img/'+this.ra[i]+'.jpg';
+        r.audioPalabra = '/assets/aupalabra/'+this.ra[i]+'.jpg';
+        r.audioSilabas = '/assets/ausilaba/'+this.ra[i]+'.jpg';
+        r.audioLetras = '/assets/auletra/'+this.ra[i]+'.jpg';
 
         arr.push(r);
       }
@@ -180,27 +169,16 @@ export class Pregunta1Page implements OnInit {
 
     crearPreguntas() //Inserta en el arreglo bidimensional las preguntas por grupo 
     {
-      var pr = this.arreglo_recursos.length/4;//pondras 5 aca guambra
-      var cont=0;
+      var pr = this.nimg/4;
       var matrix = [];
-      for(let i =0; i<this.arreglo_recursos.length;i++ )
-      {
-        
-        
+      for(let i =0; i<this.nimg;i++ ) {
 
-        var x: recurso [] = this.asignarArreglos(i,(i+pr));
+        var x: recurso [] = this.arreglo_recursos.slice(i,(i+pr));
 
         this.arreglo_preguntas.push(x);
         console.log(this.arreglo_preguntas);
-
-        cont++;
         i=i+pr-1;
-
-
       } 
-
-    
-      
     }
 
     obtenerImagenesPregunta(n: number) // genera un arreglo de strings que va a ser las fuentes para las 3 imagenes de la pregunta
