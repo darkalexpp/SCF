ffac844aa9bad25fa3a64c9a2ed8e04c982fca6b
darkalexpp <darkalexpp@gmail.com>
2021-08-03 21:43:55 -0500

    Agregar menu oculto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,29 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(private router: Router) {}
+  cnt: number;
+
+  constructor(private router: Router, private menu: MenuController) {
+    this.menu.swipeGesture(true);
+    this.cnt=0;
+   }
 
   navigate(){
+    this.menu.swipeGesture(false);
     this.router.navigate(['/pregunta1'])
   }
 
+  clickConfig() {
+    alert("hola");
+  }
 
+  hidenmenu() {
+   this.cnt++
+   if (this.cnt===10){
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+    this.cnt=0;
+   }
+
+  }
 }
