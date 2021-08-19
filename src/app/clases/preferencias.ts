cb90ee63e8fead496f44b061f85a59fe7199d6f5
darkalexpp <darkalexpp@gmail.com>
2021-08-19 15:54:38 -0500

    Fix ortografía assets

--- src/app/clases/preferencias.ts
+++ src/app/clases/preferencias.ts
@@ -1,19 +1,19 @@
 
 export class preferencias{
 
-public static BaseRecursosAll: string[] = ['araña','arpa','banana','barco','buho','caballo','cangrejo','caramelo','carro','carta','casa','cebolla','coco','cocodrilo','computadora','conejo','dado','durazno','escalera','escoba','fresa','leche','leon','limon','llama','loro','mama','manzana','mapa','melocoton','melon','murcielago','naranja','oso','oveja','paloma','pera','piña','sandalia','sandia','sol','sopa','telefono','teta','uva'];
-public static CFActividades: number = 4;
-public static CSActividades: number = 4;
-public static CISActividades: number = 4;
-public static random: boolean = true;   
-public static txtsize: number = 100; 
+    public static BaseRecursosAll: string[] = ['araña','arpa','banana','barco','búho','caballo','cangrejo','caramelo','carro','carta','casa','cebolla','coco','cocodrilo','computadora','conejo','dado','durazno','escalera','escoba','fresa','leche','león','limón','llama','loro','mama','manzana','mapa','melocotón','melón','murciélago','naranja','oso','oveja','paloma','pera','piña','sandalia','sandia','sol','sopa','teléfono','teta','uva'];
+    public static CFActividades: number = 4;
+    public static CSActividades: number = 4;
+    public static CISActividades: number = 4;
+    public static random: boolean = true;   
+    public static txtsize: number = 100; 
 
-public static rnCF=[1,2,4,5,8,11,12,13,15,16,17,18];
-public static rnCS=[0,1,2,3,4,5,6,7,8,9,10,11];
-public static raCF= ['araña','banana','caramelo','cangrejo','escalera','escoba','cebolla','murcielago','oso','pera','sandalia','telefono','carro','dado','fresa'];
-public static raCS= ['barco','cangrejo','caramelo','conejo','escalera','escoba','cocodrilo','murcielago','oso','pera','sandalia','telefono'];
-public static raCI2 = [['luna','rama'],['cuna','cama']];
-public static raCI1= ['barco','cangrejo','caramelo','conejo','escalera','escoba'];
+    public static rnCF=[1,2,4,5,8,11,12,13,15,16,17,18];
+    public static rnCS=[0,1,2,3,4,5,6,7,8,9,10,11];
+    public static raCF= ['araña','banana','caramelo','cangrejo','escalera','escoba','cebolla','murcielago','oso','pera','sandalia','telefono','carro','dado','fresa'];
+    public static raCS= ['barco','cangrejo','caramelo','conejo','escalera','escoba','cocodrilo','murcielago','oso','pera','sandalia','telefono'];
+    public static raCI2 = [['luna','rama'],['cuna','cama']];
+    public static raCI1= ['barco','cangrejo','caramelo','conejo','escalera','escoba'];
 }
 
 
